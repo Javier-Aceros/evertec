0f9ff790edd8baacb1dc2ff98a2fd9dbeabbada7
Javier_Aceros <Javier_Aceros@hotmail.com>
2020-01-14 09:26:30 -0500

    Configuraciones

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function response($datos, $code){
+		return response()->json($datos, $code);
+	}
+
+    /**
+     * @param array datos Mensajes devueltos por el validador de Laravel
+     */
+    protected function validationError($datos){
+        return $this->response(['success' => false, 'data' => $datos], 200);
+	}
+
+    /**
+     * @param string mensaje Mensaje de respuesta que se mostrara al cliente
+     * @param array datos La información devuelta al cliente
+     */
+    protected function successResponse($mensaje = null, $datos = null){
+        return $this->response(['success' => true, 'mensaje' => $mensaje, 'data' => $datos], 200);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', ['as' => '/', 'uses' => 'OrderController@showView']);
+
+Route::post('createOrder', ['as' => 'createOrder', 'uses' => 'OrderController@createOrder']);
+
